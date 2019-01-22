aa05214dae3478bc213c7e77ff3bbed831f9df65
horse <littlehorseboy@gmail.com>
2019-01-22 11:36:26 +0800

    feat: JSONFormat.jsx

---
+++ src/JSONFormat.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class JSONFormat extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      JSONString: '',
+      JSONFormatString: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      JSONString: e.target.value,
+    });
+
+    let string = '';
+    try {
+      string = JSON.stringify(JSON.parse(e.target.value), null, 2);
+      this.setState({
+        JSONFormatString: string,
+      });
+    } catch (e) {
+      this.setState({
+        JSONFormatString: '無法解析',
+      });
+
+      console.error(e);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>JSONFormat</h1>
+        <p></p>
+        <textarea
+          value={this.state.JSONString}
+          onChange={this.handleChange}
+          cols="60"
+          rows="30">
+        </textarea>
+        {' => '}
+        <textarea
+          value={this.state.JSONFormatString}
+          cols="60"
+          rows="30"
+          readOnly>
+        </textarea>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <JSONFormat />,
+  document.querySelector('#app'),
+);
